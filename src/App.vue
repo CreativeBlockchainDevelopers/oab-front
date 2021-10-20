4aae809482e0ea8d65d20503f9e083b407da8360
Antoine Brûlé <antoine.brule@netc.eu>
2021-10-20 16:45:19 +0200

    implement basic layout and connect button

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,68 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import ConnectButton from './components/ConnectButton.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div className="wrapper">
+    <header>
+      <a href="#">
+        <h1>open art blocks</h1>
+      </a>
+      <ConnectButton />
+    </header>
+    <div className="content">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae dignissimos dolor, recusandae sint aut praesentium iste fuga dolore officiis quis incidunt illum. Modi dicta explicabo reprehenderit earum similique quia eos.</p>
+    </div>
+    <footer>open art blocks</footer>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: sans-serif;
+  color: #ccc;
+  padding: 0;
+  margin: 0;
+}
+</style>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #222;
+  color: #ddd;
+  padding: 1rem;
+}
+
+header > a {
+  color: inherit;
+  text-decoration: none;
+}
+
+header h1 {
+  margin: 0;
+  font-weight: 300;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background: #111;
+}
+
+.content {
+  flex: 1;
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 1rem;
+}
+
+footer {
+  background: #222;
+  color: #ddd;
+  padding: 1rem;
 }
 </style>
---
+++ src/components/ConnectButton.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+// import Web3 from "web3";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: "_",
+    }
+  },
+};
+
+const web3Modal = new Web3Modal({
+  network: "ropsten", // optional
+  cacheProvider: false, // optional
+  providerOptions // required
+});
+
+let provider;
+
+async function connect() {
+  try {
+    provider = await web3Modal.connect();
+  } catch (e) {
+    console.log("Could not get a wallet connection", e);
+    return;
+  }
+}
+
+// const web3 = new Web3(provider);
+</script>
+
+<template>
+  <button @click="connect">Connect</button>
+</template>
+
+<style scoped>
+button {
+  background: #ddd;
+  border: 0;
+  border-radius: 2em;
+  padding: 0.5rem 1rem;
+  text-transform: uppercase;
+  font-weight: 500;
+  letter-spacing: 0.05rem;
+  transition: background 0.3s;
+}
+
+button:hover {
+  cursor: pointer;
+  background: #ccc;
+}
+</style>
